37d91ac350df8d6160a32a28e6430539a44f2ed6
Melike Ates Karayol <melikeateskarayol@gmail.com>
2019-12-03 02:43:28 +0300

    Implement List Communities Page #24
    repository for list communities

--- CommunityBuilder/src/main/java/com/mates/demo/repository/CommunityRepository.java
+++ CommunityBuilder/src/main/java/com/mates/demo/repository/CommunityRepository.java
@@ -4,9 +4,13 @@ import com.mates.demo.domain.Community;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 public interface CommunityRepository extends MongoRepository<Community, Long>  {
 
 	@Query("{name:'?0'}")
 	public Community findByName(String name);
+
+	public List<Community> findByNameLike(String keyword);
 
 }
