0bf58e442ad9366b1a98887e9914d00e12e336f3
Melike Ates Karayol <melikeateskarayol@gmail.com>
2019-12-17 22:53:17 +0300

    List posts on community home page #30
    Content of post card enhanced.

--- community-builder-web/src/forms/PostCard.js
+++ community-builder-web/src/forms/PostCard.js
@@ -7,22 +7,39 @@ import { Link } from "react-router-dom";
 
 
 const PostCard = ({ postList, showHandler}) => {
+
   return (
     <div className="communityCardClass">
         <CardColumns>
             {
                 postList.map((val, idx) =>  (
-                    <Card>
-                        <CardHeader tag="h6">{idx}</CardHeader>
-                        <CardBody>
-                              <CardText>{JSON.stringify(postList[idx].fieldValueMap)}</CardText>
-                            
-                        </CardBody>
-                    </Card>      
+                    <PostCardInfo item = {postList[idx].fieldValueMap}></PostCardInfo>
                 ))
             }
         </CardColumns>
       </div>
+  );
+};
+
+const PostCardInfo = ({item, id}) => {
+
+  return (
+    
+      <Card>
+          <CardHeader tag="h6">POST</CardHeader>
+          <CardBody>
+            {
+              Object.keys(item).map(function(key){
+                // convert key to label
+                var label = key.split('_').join(' ');
+                label = label[0].toUpperCase() + label.slice(1);
+
+                return <CardText>{label}: {item[key]}</CardText>
+              })
+            }
+          </CardBody>
+      </Card> 
+    
   );
 };
 
