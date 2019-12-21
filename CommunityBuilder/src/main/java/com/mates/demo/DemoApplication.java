b6c19cae18f6b753709496c34991328178b484d5
mateskarayol <melikeateskarayol@gmail.com>
2019-12-21 22:06:07 +0300

    Make deployment integration with heroku #33
    cors configuration and port info is added.

--- CommunityBuilder/src/main/java/com/mates/demo/DemoApplication.java
+++ CommunityBuilder/src/main/java/com/mates/demo/DemoApplication.java
@@ -2,6 +2,13 @@ package com.mates.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,27 +17,17 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-/*
 	@Bean
-	CommandLineRunner init(UserRepository userRepository) {
-
-		return args -> {
-
-			User user = new User();
-			user.setName("Mert");
-			user.setSurname("Karayol");
-			user.setUsername("mkarayol567");
-
-			userRepository.save(user);
-
-			User currentUser = userRepository.findAll().get(1);
-
-			System.out.println(currentUser.getName() + " " + currentUser.getSurname());
-
-
-		};
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// Don't do this in production, use a proper list  of allowed origins
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
-
- */
 }
