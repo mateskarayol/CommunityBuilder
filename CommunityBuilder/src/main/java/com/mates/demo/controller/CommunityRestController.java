50f489fca3326d34a6ccc6526221980196f07607
mateskarayol <melikeateskarayol@gmail.com>
2019-12-22 00:04:30 +0300

    request method changed

--- CommunityBuilder/src/main/java/com/mates/demo/controller/CommunityRestController.java
+++ CommunityBuilder/src/main/java/com/mates/demo/controller/CommunityRestController.java
@@ -7,8 +7,9 @@ import com.mates.demo.service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,9 @@ public class CommunityRestController {
 		return response;
 	}
 
-	@PostMapping("/saveCommunity")
+	@RequestMapping(value = "/saveCommunity",
+					produces = "application/json",
+					method= RequestMethod.POST)
 	@ResponseBody
 	public CommunityServiceResponse saveCommunity(@RequestBody CommunityServiceRequest request) {
 
